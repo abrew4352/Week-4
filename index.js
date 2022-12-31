723b7b62a2f0a8a67ed7ffe163ace3e1a84774bd
abrew4352 <abrew4352@gmail.com>
2022-12-30 21:21:03 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+function greet(name, height, country) {
+   alert(' Hello ' + name + height + country);   
+   }
+   
+   greet(' Ashlie ' , ' who is 70 inches tall ', ' and from the United States of America ');
+
+
+
+function greet(name, height, country) {
+   console.log(' Hello ' + name + height + country);   
+   }
+   
+   greet(' Ashlie ' , ' who is 70 inches tall ', ' and from the United States of America ');
